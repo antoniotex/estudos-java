64a2347e88fe0e32af4c208171c881f61f8497e4
antoniotex <acm.skt@gmail.com>
2018-02-04 21:55:27 -0200

    Primeiro commit

---
+++ ImprimindoVars.java
@@ -0,0 +1,12 @@
+public class ImprimindoVars {
+    //Comentário de uma linha
+    /*Coemntário de várias linhas*/
+
+    /***
+     *
+     * @param args (comentário oficial)
+     */
+    public static void main(String[] args){
+
+    }
+}
